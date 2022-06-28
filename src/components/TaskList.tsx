b9d29ff9b57414c9d180b1579fc372415e9b7715
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-28 12:31:39 -0300

    task list

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,6 +12,11 @@ export function TaskList() {
           <p>Concluídas <span>0</span></p>
         </div>
       </div>
+      <div className={styles.contentTask}>
+        <img src={clipboard} alt="Clipboard" />
+        <p className={styles.boldText}>Você ainda não tem tarefas cadastradas</p>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+      </div>
     </>
   )
 }
