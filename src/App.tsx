dbdc0543330838b9d9afbe4e29d9dbe6827066ed
Juan Savedra dos Santos <juansavedra827@gmail.com>
2022-06-28 14:13:52 -0300

    create task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Header } from './components/Header';
 import { TaskList } from './components/TaskList';
-import { TasksInfo } from './components/TasksInfo';
-import { CreateTask } from './components/CreateTask';
 import styles from './App.module.css';
 import './global.css';
 
@@ -10,8 +8,6 @@ export function App() {
     <div>
       <Header />
       <div className={styles.container}>
-        <CreateTask />
-        <TasksInfo />
         <TaskList />
       </div>
     </div>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,7 +1,9 @@
+import plus from '../assets/plus.svg';
 import trash from '../assets/trash.svg';
 import styles from './TaskList.module.css';
 import clipboard from '../assets/Clipboard.svg';
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 interface Task {
   id: string;
@@ -14,31 +16,81 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
   const [createdTasks, setCreatedTasks] = useState(0);
   const [concludedTasks, setConcludedTasks] = useState(0);
+  const [haveTask, setHaveTask] = useState(false);
 
-  const haveTask = true;
+  function handleCreateNewTask() {
+    if (!newTaskTitle) { return }
+
+    const newTask = {
+      id: uuid(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
+    setCreatedTasks(createdTasks + 1);
+    setNewTaskTitle('');
+    setHaveTask(true);
+  }
+
+  function handleToggleTaskCompletion(id: number) {
+    
+  }
+
+  function handleRemoveTask(id: number) {
+    
+  }
+
+  function Header() {
+    return (
+      <>
+        <div className={styles.tasks}>
+          <form>
+            <input 
+              type="text" 
+              placeholder="Adicione uma nova tarefa"
+              onChange={(e) => setNewTaskTitle(e.target.value)}
+              value={newTaskTitle}
+            />
+            <button type="submit" onClick={handleCreateNewTask}>
+              <div>
+                <span>Criar</span> 
+                <img src={plus} alt="Plus" />
+              </div>
+            </button>
+          </form>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.createdTasks}>
+            <p>Tarefas criadas <span>{createdTasks}</span></p>
+          </div>
+          <div className={styles.concludedTasks}>
+            <p>Concluídas <span>{concludedTasks} de {createdTasks}</span></p>
+          </div>
+        </div>
+      </>
+    )
+  }
 
   if (haveTask) {
     return (
-      <ul>
-        <li className={styles.tasksContent}>
-          <input type="checkbox" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit repudiandae soluta, non delectus  in, modi.
-          </p>
-          <a href="#"><img src={trash} alt="Trash" /></a>
-        </li>
-        <li className={styles.tasksContent}>
-          <input type="checkbox" />
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit repudiandae soluta, non delectus  in, modi.
-          </p>
-          <a href="#"><img src={trash} alt="Trash" /></a>
-        </li>
-      </ul>
+      <>
+        <Header />
+        <ul>
+          {tasks.map(task => (
+            <li className={styles.tasksContent}>
+              <input type="checkbox" />
+              <p>{task.title}</p>
+              <a href="#"><img src={trash} alt="Trash" /></a>
+            </li>
+          ))}
+        </ul>
+      </>
     )
   } else {
     return (
       <>
+        <Header />
         <div className={styles.contentTask}>
           <img src={clipboard} alt="Clipboard" />
           <p className={styles.boldText}>Você ainda não tem tarefas cadastradas</p>
